2800b39ababb899e742fb268f6a4db7735b67075
Jake <jakewarrenblack01@gmail.com>
2023-11-30 22:03:40 +0000

    Can now click the 'Roll' button to roll the dice, and the value is changed on-screen

--- src/main/java/com/example/obstaclecourse/Controllers/diceController.java
+++ src/main/java/com/example/obstaclecourse/Controllers/diceController.java
@@ -1,5 +1,18 @@
 package com.example.obstaclecourse.Controllers;
-
+import com.example.obstaclecourse.Models.Dice;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 public class diceController {
-    public void roll(){}
+    Dice dice = new Dice();
+
+    @FXML
+    private Text diceValues;
+
+    public void roll(){
+        dice.roll();
+
+        // update the text in the dice view
+        diceValues.setText(Integer.toString(dice.getValue()));
+    }
 }
